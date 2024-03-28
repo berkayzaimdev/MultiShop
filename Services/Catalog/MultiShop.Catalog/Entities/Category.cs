1d555fcdeb5e56e83edb4b2ab40ff475b0dc2494
berkayzaimdev <berkayzaimdev@gmail.com>
2024-03-28 07:40:49 +0300

    Added Category and Product entities

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId] // MongoDB için ID tanımlaması
+        [BsonRepresentation(BsonType.ObjectId)] // ID için enum kullandık
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
