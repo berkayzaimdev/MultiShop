0c5ce9cd2951feccedb7d85b13b652adca4ca898
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 15:42:07 +0300

    Created Basket service Added BasketItem and BasketTotal DTOs

---
+++ Services/Basket/MultiShop.Basket/Dtos/BasketTotalDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Basket.Dtos
+{
+    public class BasketTotalDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int DiscounRate { get; set; }
+        public ICollection<BasketItemDto> BasketItems { get; set; } = [];
+        public decimal TotalPrice { get => BasketItems.Sum(x => x.Price * x.Quantity); }
+    }
+}
