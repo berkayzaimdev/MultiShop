fb04e216fc0ced0a680e218151d344f9a3168ffc
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 15:58:15 +0300

    Added RedisSettings and RedisService

---
+++ Services/Basket/MultiShop.Basket/Services/RedisService.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Services
+{
+    public class RedisService
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        private ConnectionMultiplexer _connectionMultiplexer;
+
+        public RedisService(string host, string port)
+        {
+            Host = host;
+            Port = port;
+        }
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{Host}:{Port}");
+        public IDatabase GetDb(int db=1) => _connectionMultiplexer.GetDatabase(0);
+    }
+}
