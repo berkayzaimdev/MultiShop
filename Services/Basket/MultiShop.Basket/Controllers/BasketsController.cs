e9c8e220e8a0ff0193fe4cf1044989be72cc7a9d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 16:23:22 +0300

    Added BasketsController

--- Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Services.Abstract;
 
 namespace MultiShop.Basket.Controllers
 {
@@ -7,5 +9,35 @@ namespace MultiShop.Basket.Controllers
     [ApiController]
     public class BasketsController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketsController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasketDetailAsync()
+        {
+            var value = await _basketService.GetBasketAsync(_loginService.GetUserId);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveBasketAsync(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+            await _basketService.SaveBasketAsync(basketTotalDto);
+            return Ok("Değişiklikler başarıyla kaydedildi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasketAsync(BasketTotalDto basketTotalDto)
+        {
+            await _basketService.DeleteBasketAsync(_loginService.GetUserId);
+            return Ok("Sepet silindi!");
+        }
     }
 }
