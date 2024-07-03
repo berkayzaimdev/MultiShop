25e3bfbdeb489f4ea9f82bcf5e54d91264ce9ce3
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 16:09:49 +0300

    Added LoginService

---
+++ Services/Basket/MultiShop.Basket/Services/Concrete/LoginService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Basket.Services.Abstract;
+
+namespace MultiShop.Basket.Services.Concrete
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public string GetUserId => _contextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
