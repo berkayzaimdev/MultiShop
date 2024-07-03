94872b3b450d59677387f0d041a5a3810a0c386b
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 16:07:17 +0300

    Added BasketService

---
+++ Services/Basket/MultiShop.Basket/Services/Abstract/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services.Abstract
+{
+    public interface IBasketService
+    {
+        Task<BasketTotalDto> GetBasketAsync(string userId);
+        Task SaveBasketAsync(BasketTotalDto basket);
+        Task DeleteBasketAsync(string userId);
+    }
+}
---
+++ Services/Basket/MultiShop.Basket/Services/Concrete/BasketService.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Services.Abstract;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services.Concrete
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<BasketTotalDto> GetBasketAsync(string userId)
+        {
+            var basket = await _redisService.GetDb().StringGetAsync(userId);
+            return JsonSerializer.Deserialize<BasketTotalDto>(basket)!;
+        }
+
+        public async Task SaveBasketAsync(BasketTotalDto basket)
+        {
+            await _redisService.GetDb().StringSetAsync(basket.UserId, JsonSerializer.Serialize(basket));
+        }
+
+        public async Task DeleteBasketAsync(string userId)
+        {
+            await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+    }
+}
