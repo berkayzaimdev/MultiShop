d227b0aa84a408ed277c6c90af2037bbdb2da4e8
berkayzaimdev <berkayzaimdev@gmail.com>
2024-07-03 10:16:51 +0300

    Added IGenericDal

---
+++ Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+    }
+}
